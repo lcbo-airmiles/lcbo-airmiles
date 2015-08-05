4ac9361afa0419cd426c2f3bf5083331fee7c185
James McDermid <hello@jamesmcdermid.ca>
2015-08-05 16:11:23 -0400

    LCBO call for airmiles works

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,20 @@ var jamesAPI = 'MDo1ZmMzNGQ0Yy0zYWVmLTExZTUtODFkYi02YmQ0ZWM1NzJlOTQ6RDNTeEVIS1M4
 
 app.test = function(){
 	$.ajax({
-		url: 'http://lcboapi.com/stores',
+		url: 'http://lcboapi.com/products',
 		type: 'GET',
 		dataType: 'jsonp',
-		headers: {
-			Authorization: jamesAPI
+		data: {
+			access_key: jamesAPI,
+			per_page: 100,
+			where: 'has_bonus_reward_miles',
+			where_not: 'is_dead'
 		}
 	}).then(function(data) {
-		console.log(data);
+
+		console.log(data.result);
+	
+		
 	});//end results function
 }; //end TEST function
 
