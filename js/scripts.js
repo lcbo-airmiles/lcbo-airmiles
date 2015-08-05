86c45ddba117f9f04379e681d643800746b009c0
James McDermid <hello@jamesmcdermid.ca>
2015-08-05 18:10:40 -0400

    Added the geo request to the LCBO app

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,45 @@
 var app = {};
 
 var jamesAPI = 'MDo1ZmMzNGQ0Yy0zYWVmLTExZTUtODFkYi02YmQ0ZWM1NzJlOTQ6RDNTeEVIS1M4Zlh1M0E1UUZjMlFuRzFMWkhzbzcyeUQ2bnRN';
+var userInput = 'N1L1L6';
 
 
-
-app.test = function(){
+app.products = function(){
 	$.ajax({
 		url: 'http://lcboapi.com/products',
 		type: 'GET',
 		dataType: 'jsonp',
 		data: {
-			access_key: jamesAPI,
+			access_key: app.jamesAPI,
 			per_page: 100,
 			where: 'has_bonus_reward_miles',
 			where_not: 'is_dead'
 		}
 	}).then(function(data) {
 
-		console.log(data.result);
-	
+		//console.log(data.result);
 		
 	});//end results function
 }; //end TEST function
+
+app.stores = function(){
+	$.ajax({
+		url: 'http://lcboapi.com/stores',
+		type: 'GET',
+		dataType: 'jsonp',
+		data: {
+			access_key: app.jamesAPI,
+			per_page: 5,
+			geo: userInput
+			//lat: 43.647777,
+			//lon:-79.369978
+		}
+	}).then(function(data) {
+
+		console.log(data.result);
+		
+	});//end results function
+}
 
 
 
@@ -34,7 +52,8 @@ app.test = function(){
 // INIT FUNCTION
 // =============================================================================
 app.init = function(){
-	app.test();
+	app.products();
+	app.stores();
 }; // end init function
 
 // =============================================================================
