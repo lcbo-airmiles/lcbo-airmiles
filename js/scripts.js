1fd1e22ba0a41560f9d22b5153071cae941389a6
Alex Davey <alex@alexdavey.ca>
2015-08-07 14:35:06 -0400

    added the jquery for mapbox- NOTE deleted the lcbo temporarily.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,9 @@
 // =============================================================================
 // DECLARE GLOBAL APP OBJECT & variables
 // =============================================================================
-var app = {};
-
-var jamesAPI = 'MDo1ZmMzNGQ0Yy0zYWVmLTExZTUtODFkYi02YmQ0ZWM1NzJlOTQ6RDNTeEVIS1M4Zlh1M0E1UUZjMlFuRzFMWkhzbzcyeUQ2bnRN';
 
 
 
-app.test = function(){
-	$.ajax({
-		url: 'http://lcboapi.com/products',
-		type: 'GET',
-		dataType: 'jsonp',
-		data: {
-			access_key: jamesAPI,
-			per_page: 100,
-			where: 'has_bonus_reward_miles',
-			where_not: 'is_dead'
-		}
-	}).then(function(data) {
-
-		console.log(data.result);
-	
-		
-	});//end results function
-}; //end TEST function
 
 
 //1. We want the user to enter their postal code.
@@ -32,9 +11,27 @@ app.test = function(){
 //2. (as another option)We want the user to enable "geo location" to receive their location via 
 //GoogleMaps / Map Box, by clicking a button.
 
-//3. We want to 'smooth scroll' their results (whichever method they selected) further down the page. 
+//3. We want to 'smooth scroll' their results (whichever method they selected) further down the page.
+
 //4. We want to return 3 LCBO locations within their postal code parameters.
+//4a) We will make an AJAX call to LBCO to bring back their locations
+//4b) We will drop the LCBO locations in proximity to their postal code or geolocation.
+
 //5. We want to return a map displaying their LCBO locations using markers.
+
+//use jquery to hide the div - > SHOW the hidden div BEFORE the map is revealed, make
+//the map slide in after we have shown the div. make the div first AND THEN put the map on the page.
+
+var app = {};
+//LEAFLET MAPBOX
+var alexID = 'alexandradavey.n42d3egc';
+var alexMap = 'https://a.tiles.mapbox.com/v4/alexandradavey.n42d3egc/page.html?access_token=pk.eyJ1IjoiYWxleGFuZHJhZGF2ZXkiLCJhIjoiNWI5NWYzY2Q0NTQyYjYyMmFjNWY5ZWEwZGE5MjAxZWMifQ.yQUY4RtfbkaeoUlcbsxy8g#4/45.89/-75.63';
+var alexkey = 'pk.eyJ1IjoiYWxleGFuZHJhZGF2ZXkiLCJhIjoiNWI5NWYzY2Q0NTQyYjYyMmFjNWY5ZWEwZGE5MjAxZWMifQ.yQUY4RtfbkaeoUlcbsxy8g';
+L.mapbox.accessToken = alexkey;
+app.map = L.mapbox.map('map',alexID).setView([44.129, -79.306], 7);
+
+
+
 
 //6. Once a store has been selected by the user, we will 'smooth scroll' to display the airmiles promotions, further down the page.
 
@@ -48,7 +45,7 @@ app.test = function(){
 // INIT FUNCTION
 // =============================================================================
 app.init = function(){
-	app.test();
+
 }; // end init function
 
 // =============================================================================
